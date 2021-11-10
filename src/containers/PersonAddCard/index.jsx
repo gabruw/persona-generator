a031b4ef6acd91712667db61e0c0dc0a69543c65
gabruw <gabrielmarques.mg@gmail.com>
2021-11-10 00:34:27 -0300

    feat: PersonAddCard

---
+++ src/containers/PersonAddCard/index.jsx
@@ -0,0 +1,45 @@
+//#region Imports
+
+import React, { useCallback } from 'react';
+import { Card, Icon, Loader } from 'semantic-ui-react';
+import usePersonService from 'services/person/usePersonService';
+import usePersonContext from 'storages/person/context';
+import personFormat from 'utils/formatters/person';
+import useAvatar from 'utils/hooks/useCreateAvatar';
+import styles from './styles.module.css';
+
+//#endregion
+
+const PersonAddCard = () => {
+    const { create } = useAvatar();
+
+    const { addPerson } = usePersonContext();
+    const { requestState, fetchPerson } = usePersonService();
+
+    const fetchNewPerson = useCallback(async () => {
+        const { data } = await fetchPerson();
+
+        const image = create(data);
+        const format = personFormat(data, image);
+
+        addPerson(format);
+    }, [fetchPerson, create, addPerson]);
+
+    return (
+        <Card className={styles.card}>
+            <Card.Content className={styles.cardContent}>
+                <div className={styles.iconContainer} onClick={() => !requestState.isLoading && fetchNewPerson()}>
+                    {requestState.isLoading ? (
+                        <Loader className={styles.loader} inline='centered' size='large' active />
+                    ) : (
+                        <Icon className={styles.icon} name='plus' size='huge' />
+                    )}
+                </div>
+
+                <div className={styles.text}>Add new person</div>
+            </Card.Content>
+        </Card>
+    );
+};
+
+export default PersonAddCard;
