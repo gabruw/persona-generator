19cc1e4d7d96c77b66ee787675b41cb0bc19098e
gabruw <gabrielmarques.mg@gmail.com>
2021-11-10 00:37:38 -0300

    feat: App

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@
 import 'assets/styles/global.css';
 import React from 'react';
 import 'semantic-ui-css/semantic.min.css';
+import Person from 'views/Person';
 
 //#endregion
 
-const App = () => <></>;
+const App = () => <Person />;
 
 export default App;
