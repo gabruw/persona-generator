69c998364a099cbe9fd80708aa3e7e18cf13bf1f
gabruw <gabrielmarques.mg@gmail.com>
2021-11-10 00:34:15 -0300

    feat: PersonCardList

---
+++ src/components/PersonCardList/index.jsx
@@ -0,0 +1,32 @@
+//#region Imports
+
+import PersonCard from 'components/PersonCard';
+import PersonAddCard from 'containers/PersonAddCard';
+import React from 'react';
+import { Grid } from 'semantic-ui-react';
+import usePersonContext from 'storages/person/context';
+import styles from './styles.module.css';
+
+//#endregion
+
+const PersonCardList = () => {
+    const { people } = usePersonContext();
+
+    return (
+        <Grid className={styles.grid} centered>
+            <Grid.Row columns={6}>
+                {people.map((person, index) => (
+                    <Grid.Column className={styles.column}>
+                        <PersonCard key={index} person={person} />
+                    </Grid.Column>
+                ))}
+
+                <Grid.Column className={styles.column}>
+                    <PersonAddCard />
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>
+    );
+};
+
+export default PersonCardList;
